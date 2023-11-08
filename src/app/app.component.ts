4c3c2ceae46df67b358464590613ea884f395f53
aravind.yedala <aravind.yedala@nslhub.com>
2023-11-08 16:41:16 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,83 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'angone';
-  nums = [5,7,7,8,8,10];
-   target = 8
-  searchRange(nums:any, target:any) {
-    return [nums.indexOf(target), nums.lastIndexOf(target)];
-  }
-  data: any = [];
+  Data: any =[];
   constructor() {
-    for (let i = 0; i < 10000000; i++) {
-      this.data.push(i);
+    this.createData();
+  }
+  createData(){
+    for (let i = 0; i < 1000000; i++) {
+      let entity: any = {
+        nslAttr:[{name:'one',id:1},{name: 'two',id:2}, {name: 'three',id:3},{name: 'four',id:4}]
+      }
+      this.Data.push(entity);
     };
-
-
-
-    let results: any = [];
-    let data = this.data;
-
-    function checktwo() {
-      let t1 = performance.now();
-      var t2;
-      const worker1 = new Worker(new URL('./web1.worker.ts', import.meta.url));
-      const worker2 = new Worker(new URL('./web1.worker.ts', import.meta.url));
-      const worker3 = new Worker(new URL('./web1.worker.ts', import.meta.url));
-      const worker4 = new Worker(new URL('./web1.worker.ts', import.meta.url));
-
-      // const worker2 = new Worker('./webworker.js');
-      const halfway = Math.floor(data.length/2 );
-
-      worker1.onmessage = ({data}) =>{
-        t2 = performance.now();
-        console.log(t1,t2,new Date());
-      }
-      worker2.onmessage = ({data}) =>{
-        t2 = performance.now();
-        console.log(t1,t2,new Date());
-      }
-      worker3.onmessage = ({data}) =>{
-        t2 = performance.now();
-        console.log(t1,t2,new Date());
-      }
-      worker4.onmessage = ({data}) =>{
-        t2 = performance.now();
-        console.log(t1,t2,new Date());
-      }
-      worker1.postMessage(data.slice(0, 2500000));
-      worker2.postMessage(data.slice(2500000,5000000));
-      worker1.postMessage(data.slice(5000000, 7500000));
-      worker2.postMessage(data.slice(7500000,10000000));
-
-     
-      console.log('two');
-      
-
-    }
     this.checkone();
-    checktwo();
-
+    this.checktwo();
+    
   }
   checkone() {
     var res: any = [];
     let t1 = performance.now();
     let t2;
-    this.data.forEach((a: any, i: any) => {
-      // res.push(a+10);
-      this.searchRange(this.nums,this.target)
-
-      // if(i===10000000){
-        t2 = performance.now();
-      // }
+    this.Data.forEach((ent: any, i: any) => {
+      ent.nslAttr.forEach((attr: any) => {
+        res.push(attr.id);
+      })
     });
+    t2 = performance.now();
     console.log('one');
-    console.log(t1, t2);
+    console.log(t1, t2,t2-t1);
   }
-  
+   checktwo() {
+     let res: any = [];
+    let t1 = performance.now();
+    var t2;
+    const worker1 = new Worker(new URL('./web1.worker.ts', import.meta.url));
+    const worker2 = new Worker(new URL('./web1.worker.ts', import.meta.url));
+    worker1.onmessage = ({data}) =>{
+      // res = data;
+      t2 = performance.now();
+      console.log(t1,t2,t2-t1);
+    }
+    worker2.onmessage = ({data}) =>{
+
+
+      t2 = performance.now();
+      console.log(t1,t2,t2-t1);
+    }
+    // worker3.onmessage = ({data}) =>{
+    //   t2 = performance.now();
+    //   console.log(t1,t2,new Date());
+    // }
+    // worker4.onmessage = ({data}) =>{
+    //   t2 = performance.now();
+    //   console.log(t1,t2,new Date());
+    // }
+    worker1.postMessage(1);
+    worker2.postMessage(this.Data.slice(500000));
+   
+
+   
+    // console.log('two');
+    
+
+  }
 }
--- src/app/web1.worker.ts
+++ src/app/web1.worker.ts
@@ -3,17 +3,13 @@
 addEventListener('message', ({ data }) => {
   const response = data;
   let result: any = [];
-  let nums = [5,7,6,78,8,10];
-  let target = 8
- function searchRange(nums:any, target:any) {
-   return [nums.indexOf(target), nums.lastIndexOf(target)];
- }
-  response.forEach((res: any) => {
-    result.push(res-10);
-    searchRange(nums,result);
-    // console.log(res);
-  })
+  // response.forEach((res: any) => {
+  //   res.nslAttr.forEach((attr: any) => {
+  //     result.push(attr.id);
+  //   })
+  //   // console.log(res);
+  // })
   
-  postMessage(result);
+  postMessage(data);
   // return response.map((item: any) => item + 10);
 });
